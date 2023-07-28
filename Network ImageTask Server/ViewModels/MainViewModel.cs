99f2132c99845117bfef584b0b88266c5da55301
Azizo_pu01 <Azizo_pu01@STHQ0129-13>
2023-07-28 14:27:20 +0400

    T

--- Network ImageTask Server/ViewModels/MainViewModel.cs
+++ Network ImageTask Server/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Network_ImageTask_Server.Views.UserControls;
+﻿using Network_ImageTask_Server.Commands;
+using Network_ImageTask_Server.Views.UserControls;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -7,43 +8,51 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace Network_ImageTask_Server.ViewModels
 {
     public class MainViewModel
     {
+        public RelayCommand SendButtonCommand { get; set; }
         public MainViewModel()
         {
-            var ipAdress = IPAddress.Parse("192.168.0.109");
-            var port = 27001;
-
-            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            SendButtonCommand = new RelayCommand((_) =>
             {
-                var ep = new IPEndPoint(ipAdress, port);
-                socket.Listen(10);
-                socket.Bind(ep);
-                while (true)
+                SenderImage senderImage = new SenderImage();
+                SenderImageViewModel senderImageViewModel = new SenderImageViewModel();
+                senderImage.DataContext = senderImageViewModel;
+                Task.Run(() =>
                 {
-                    var client = socket.Accept();
-                    SenderImage senderImage = new SenderImage();
-                    SenderImageViewModel senderImageViewModel = new SenderImageViewModel();
-                    senderImage.DataContext = senderImageViewModel;
-                    Task.Run(() =>
-                    {
-                        var length = 0;
-                        var bytes = new byte[500000];
+                    var ipAdress = IPAddress.Parse("10.2.29.13");
+                    var port = 27014;
 
-                        do
+                    using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                    {
+                        var ep = new IPEndPoint(ipAdress, port);
+                        socket.Bind(ep);
+                        socket.Listen(10);
+                        while (true)
                         {
-                            length = client.Receive(bytes);
-                            var image = Encoding.UTF8.GetString(bytes);
-                            senderImageViewModel.Image = image;
-                            App.MainStackPanel.Children.Add(senderImage);
-                        } while (length > 0);
-                    });
-                }
-            }
+                            var client = socket.Accept();
+                            MessageBox.Show($"{client.RemoteEndPoint}");
+
+                            var length = 0;
+                            var bytes = new byte[34000];
+
+                            do
+                            {
+                                length = client.Receive(bytes);
+                                var image = Encoding.ASCII.GetString(bytes);
+                                senderImageViewModel.Image = "C:\\\\Users\\\\Azizo_pu01\\\\Downloads\\\\TASKSYSTEM2.png";
+                            } while (length > 0);
+
+                        }
+                    }
+                });
+                App.MainStackPanel.Children.Add(senderImage);
+            });
         }
     }
 }
